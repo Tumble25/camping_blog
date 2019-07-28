d071bd43c30d89d749321936e6b74d43f4e9b69b
Rob Tuinte <robtuinte15@gmail.com>
2019-07-28 19:01:38 +0200

    models finished

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_163603) do
+ActiveRecord::Schema.define(version: 2019_07_28_165252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.bigint "interview_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["interview_id"], name: "index_comments_on_interview_id"
+  end
 
   create_table "interview_photos", force: :cascade do |t|
     t.string "picture"
@@ -45,5 +53,6 @@ ActiveRecord::Schema.define(version: 2019_07_28_163603) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "interviews"
   add_foreign_key "interview_photos", "interviews"
 end

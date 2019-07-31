8a4e157bd7a164b789c80959cf426f1ff9375a75
Rob Tuinte <robtuinte15@gmail.com>
2019-07-31 14:54:56 +0200

    test

--- app/controllers/interviews_controller.rb
+++ app/controllers/interviews_controller.rb
@@ -1,5 +1,5 @@
 class InterviewsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show, :edit, :update ]
+  skip_before_action :authenticate_user!, only: [:index, :show ]
 
   def index
     @interviews = Interview.all

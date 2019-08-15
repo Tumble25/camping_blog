aa43b7b76d015e1018fd4a795f9c9757d197eeed
Rob Tuinte <robtuinte15@gmail.com>
2019-08-15 12:27:06 +0200

    responsive

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:index, :new, :create, :destroy]
   end
   resources :comments, only: [ :show, :edit, :update, :destroy ]
+  get 'over-ons', :to => 'pages#home', :as => :over_ons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
